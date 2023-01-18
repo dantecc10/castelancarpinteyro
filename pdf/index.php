56ae4df9300047ce11a42efc86a0b4d0475833f4
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-17 19:14:29 -0600

    segundo intento

--- pdf/index.php
+++ pdf/index.php
@@ -1,5 +1,15 @@
 <?php
-$mpdf = new \Mpdf\Mpdf();
+$mpdf = new \Mpdf\Mpdf([
+    'setAutoTopMargin' => 'stretch',
+    'autoMarginPadding' => 7
+]);
+
+//llame al contenido y la imagen de la marca de agua
+$mpdf->SetWatermarkText('MiEmpresa.com');
+$mpdf->showWatermarkText = true;
+$mpdf->watermarkTextAlpha = 0.1;
+
+
 $mpdf->SetHTMLHeader('<header class="bg-dark">
 <!-- Start: Hero Clean Reverse -->
 <div class="container pt-4 pt-xl-5">
@@ -22,7 +32,7 @@ $mpdf->SetHTMLHeader('<header class="bg-dark">
                     <p class="user-terminal">johndoe@admin: ~</p>
                 </div>
                 <div class="terminal_body">
-                    <div class="terminal_promt">
+                <div class="terminal_promt">
                         <span class="terminal_user">johndoe@admin:</span>
                         <span class="terminal_location">~</span>
                         <span class="terminal_bling">$</span>
@@ -55,12 +65,12 @@ $mpdf->SetHTMLFooter('<footer class="bg-dark">
                 <li><a href="#">Equipo</a></li>
                 <li><a href="#">Contacto</a></li>
             </ul>
-        </div><!-- End: About -->
+            </div><!-- End: About -->
         <!-- Start: Social Icons -->
         <div
             class="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last item social">
             <div class="fw-bold d-flex align-items-center mb-2"><span
-                    class="bs-icon-sm bs-icon-circle bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2 rueda-logo"><svg
+            class="bs-icon-sm bs-icon-circle bs-icon-primary d-flex justify-content-center align-items-center bs-icon me-2 rueda-logo"><svg
                         xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
                         viewBox="0 0 16 16" class="bi bi-code-slash">
                         <path
@@ -82,11 +92,11 @@ $mpdf->SetHTMLFooter('<footer class="bg-dark">
                             d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z">
                         </path>
                     </svg></a></li>
-            <li class="list-inline-item"><a href="https://wa.me/527979773095" target="_blank"><svg
+                    <li class="list-inline-item"><a href="https://wa.me/527979773095" target="_blank"><svg
                         class="bi bi-whatsapp" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
                         fill="currentColor" viewBox="0 0 16 16">
                         <path
-                            d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z">
+                        d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z">
                         </path>
                     </svg></a></li>
             <li class="list-inline-item"><a href="https://github.com/dantecc10" target="_blank">
@@ -123,4 +133,6 @@ $mpdf->SetHTMLFooter('<footer class="bg-dark">
 </footer>');
 
 $mpdf->WriteHTML('Hola');
-$mpdf->Output();
+
+//guarde el archivo y coloque la ubicación que necesita carpeta/nombre de archivo
+$mpdf->Output("miempresa.pdf", 'F');
