17f815476ab2334d6b0ea42c60bdabccbb12c402
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-17 22:00:05 -0600

    sobre txt

---
+++ pdf/txt.php
@@ -0,0 +1,14 @@
+<?php
+// Abriendo el archivo
+$archivo = fopen("prueba.txt", "r");
+
+// Recorremos todas las lineas del archivo
+while (!feof($archivo)) {
+    // Leyendo una linea
+    $traer = fgets($archivo);
+    // Imprimiendo una linea
+    echo nl2br($traer);
+}
+
+// Cerrando el archivo
+fclose($archivo);
