36a18641e88e3a9df76b2b1d96eeee288c0668fd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-19 23:12:27 -0600

    iniciando contrucción de consultas avanzadas de SQL

---
+++ php scripts/configsScripts.php
@@ -0,0 +1,13 @@
+<?php
+function parámetrosCarruselPrincipal()
+{
+    $apSupCont1 = '<section class="py-5">
+    <!-- Start: Carousel Hero -->
+    <div class="carousel slide" data-bs-ride="carousel" id="carousel-3">
+        <div class="carousel-inner">';
+    $ciSupCont1 = '</div>
+    <div><a class="carousel-control-prev" href="#carousel-3" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-3" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
+    <ol class="carousel-indicators">';
+    $apSupCont = "";
+    $ciSupCont = "";
+}
