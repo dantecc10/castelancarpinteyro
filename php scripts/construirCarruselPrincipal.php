5cf6d414989dfd005e37b7ba9694197a79b41594
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-22 19:38:48 -0600

    con sql directo

--- php scripts/construirCarruselPrincipal.php
+++ php scripts/construirCarruselPrincipal.php
@@ -19,10 +19,10 @@ while ($columna = mysqli_fetch_array($resultado)) {
             case 1:
                 if ($contador = 1) {
                     $carruselÍtems .= $apCont1["a"][1];
-                    $carruselIndexes .= ($apCont2 . /*($columna['id_ítem'] - 1)*/  ($contador - 1) . $ciCont2["b"][1]);
+                    $carruselIndexes .= ($apCont2 . ($columna['id_ítem'] - 1) . $ciCont2["b"][1]);
                 } else {
                     $carruselÍtems .= $apCont1["a"][0];
-                    $carruselIndexes .= ($apCont2 . /*($columna['id_ítem'] - 1)*/  ($contador - 1) . $ciCont2["b"][0]);
+                    $carruselIndexes .= ($apCont2 . ($columna['id_ítem'] - 1) . $ciCont2["b"][0]);
                 }
                 break;
             case 2:
