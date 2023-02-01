6dba4e4b59b4b8cfce5a3a50b7a97aebe00f1e82
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-01-31 21:58:15 -0600

    es mx

--- aprende/artículos/1/cómo-crear-un-repositorio-de-github/index.php
+++ aprende/artículos/1/cómo-crear-un-repositorio-de-github/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es-MX">
 
 <head>
     <meta charset="utf-8">
@@ -7,7 +7,10 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HV59T08TYC"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'G-HV59T08TYC');
@@ -15,28 +18,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Carta a mis padres - Dante Castelán Carpinteyro</title>
     <meta name="theme-color" content="#5436cd">
-    <meta name="description"
-        content="Programación; artículos, tutoriales y cursos disponibles para entusiastas de la programación en Castelán Carpinteyro.">
+    <meta name="description" content="Programación; artículos, tutoriales y cursos disponibles para entusiastas de la programación en Castelán Carpinteyro.">
     <meta name="twitter:description" content="Cursos de programación y servicios de hosting web">
     <meta name="twitter:title" content="Castelán Carpinteyro">
     <meta name="author" content="Dante Castelán Carpinteyro">
     <meta property="og:type" content="website">
-    <meta property="og:image"
-        content="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
+    <meta property="og:image" content="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:image"
-        content="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
+    <meta name="twitter:image" content="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
     <link rel="icon" type="image/png" sizes="4398x3333" href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV.png">
     <link rel="icon" type="image/png" sizes="4398x3333" href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV.png">
-    <link rel="icon" type="image/png" sizes="4405x3333"
-        href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
+    <link rel="icon" type="image/png" sizes="4405x3333" href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
     <link rel="icon" type="image/png" sizes="4398x3333" href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV.png">
-    <link rel="icon" type="image/png" sizes="4405x3333"
-        href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
+    <link rel="icon" type="image/png" sizes="4405x3333" href="https://castelancarpinteyro.com/assets/img/castelancarpinteyro/DanteDEV-Black-Blackground.png">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/bootstrap/css/bootstrap.min.css">
     <link rel="manifest" href="https://castelancarpinteyro.com/manifest.json">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/Carousel-Hero.css">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/extra.css">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/uiverse.css">
@@ -45,15 +42,10 @@
 <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
     <!-- Start: Navbar Centered Links -->
     <nav class="navbar navbar-dark navbar-expand-md sticky-top navbar-shrink py-3" id="mainNav">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span
-                    class="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon rueda-logo"><svg
-                        xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                        viewBox="0 0 16 16" class="bi bi-code-slash">
-                        <path
-                            d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span class="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon rueda-logo"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-code-slash">
+                        <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z">
                         </path>
-                    </svg></span><span>Castelán Carpinteyro</span></a><button data-bs-toggle="collapse"
-                class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle
+                    </svg></span><span>Castelán Carpinteyro</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle
                     navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav mx-auto">
@@ -62,8 +54,7 @@
                     <li class="nav-item"><a class="nav-link" href="servicios.php">Servicios</a></li>
                     <li class="nav-item"><a class="nav-link" href="products.php">Productos</a></li>
                 </ul>
-                <div class="div1-efecto"><a class="btn btn-primary shadow div2-efecto" role="button"
-                        href="registro.php">Iniciar sesión</a></div>
+                <div class="div1-efecto"><a class="btn btn-primary shadow div2-efecto" role="button" href="registro.php">Iniciar sesión</a></div>
             </div>
         </div>
     </nav><!-- End: Navbar Centered Links -->
@@ -82,43 +73,35 @@
             </div>
             <div class="row pt-5">
                 <div class="col">
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Hola, papás:
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Hola, papás:
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Aquí Dante Castelán Carpinteyro su hijo.
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Aquí Dante Castelán Carpinteyro su hijo.
                             No sé exactamente a qué hora estarán leyendo esto; pero yo sí sé a qué hora lo escribo: 1:26
                             de la tarde del martes 24 de enero del 2023.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Si bien la junta de padres de familia es a
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Si bien la junta de padres de familia es a
                             las 2:10, yo sé bién qué puedo y qué no puedo hacer; y no me habría comprometido a escribir
                             esta carta de esta forma de no ser capaz de hacerla y escribirla.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Esta carta es generada a través de un
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Esta carta es generada a través de un
                             script de PHP que yo mismo he creado; para ahorrar tiempo. Yo la escribo, sólo que mi propio
                             código hace la construcción del código HTML para copiar, pegar y tener esta presentación
                             que, me parece, es decente y un poco sofisticada; pero no los quiero aburrir con los
                             detalles técnicos, que poca gente me entiende cuando de programación se trata.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Me percibo en soledad; en inmensa soledad
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Me percibo en soledad; en inmensa soledad
                             porque casi nadie sabe de qué hablo y adoro hablar de código y programación.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Estamos en un punto en el que para ustedes
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Estamos en un punto en el que para ustedes
                             estoy sentado en la compu y no hago nada de provecho, pero no tienen idea de lo mucho que
                             aprendo y de la ventaja que le llevo y saco gracias a eso a mis maestros.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Tengo que agradecer su participación en
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Tengo que agradecer su participación en
                             eso, que me han brindado los recursos para lograrlo; mi laptop, que es mi mejor amiga y
                             aliada; el idioma inglés, en el que invirtieron bastante; y mi servidor; que me ha inspirado
                             y motivado a ser cada días más profesional.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Es claro que soy mucho, he evolucionado
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Es claro que soy mucho, he evolucionado
                             bastante, y les debo gran parte de eso. Yo no sería una persona intelectual y dedicada de no
                             ser por las conversaciones tan amenas que mantenemos sobre literatura, ciencia, cultura,
                             política y tecnología que tenemos; son diálogos en los que toda nuestra familia crece y nos
@@ -126,50 +109,41 @@
                             agradezco enormemente que lo hagan conmigo y que me den el gusto de vivir en su hogar y
                             haber recibido su educación hasta el momento.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Les diría en quejas y sugerencias, que por
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Les diría en quejas y sugerencias, que por
                             favor, me permitan ser más libre; dedicarle más tiempo a la ingeniería, y a la programación,
                             que mucho me darán a mí, y mucho les retribuirá a ustedes.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">No sé si quiero mayor o menor interacción
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">No sé si quiero mayor o menor interacción
                             social con ustedes, pero sin duda quiero que sea de calidad.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Creo que lo que haga ahora, con mi vida,
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Creo que lo que haga ahora, con mi vida,
                             con mi futuro en la universidad, será decisivo en mi vida, pero también en la suya. Tengo
                             muchos proyectos, quiero tener un empleo, luego una compañía, fundación, emprender negocios
                             y acciones de impacto social. Quiero convertirme en una figura pública y que mi ustedes; mi
                             familia sean parte de ello.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">No me veo teniendo éxito y disfrutándolo
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">No me veo teniendo éxito y disfrutándolo
                             sin que ustedes estén ahí, conmigo, respaldándome.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Es por eso que quiero expresarles mi
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Es por eso que quiero expresarles mi
                             gratitud y amor. No les diría esto en persona, porque no soy así; porque tampoco me criaron
                             así y mi personalidad se moldea según los límites, métodos y estándares que se aprenden en
                             casa. Ustedes sabrán que a veces sentarme junto a ustedes es todo lo que tengo que hacer
                             para comunicarles que los quiero y que les tengo aprecio.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Creo, también que quiero seguir siendo
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Creo, también que quiero seguir siendo
                             como soy, y no deberían tratar de hacer un cambio, o alguna modificación; considero que la
                             formación que me han dado es buena, de calidad; pero ya he adquirido mis bases de criterio;
                             lógica, sentido común y estilo de vida; hay mucho de mí que no demuestro pero que sé que es
                             bueno y en el futuro los hará sentir orgullosos.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Si bien me encanta tratar de ser siempre
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Si bien me encanta tratar de ser siempre
                             el mejor en todo, no hay nada que haga por hacerme daño o a ustedes.
                         </span><br></p>
-                    <p class="sangría" style="text-align: justify;"><span
-                            style="background-color: rgba(248, 249, 250, 0);">Seamos felices, permítanme crecer para
+                    <p class="sangría" style="text-align: justify;"><span style="background-color: rgba(248, 249, 250, 0);">Seamos felices, permítanme crecer para
                             retribuir a la familia lo que he recibido.</span><br></p>
 
-                    <p class="text-end" style="text-align: justify;"><strong><em><span
-                                    style="background-color: rgba(248, 249, 250, 0);">Jamás duden de lo mucho que los
+                    <p class="text-end" style="text-align: justify;"><strong><em><span style="background-color: rgba(248, 249, 250, 0);">Jamás duden de lo mucho que los
                                     aprecio, porque les debo casi todo a ustedes.<br><i>- Dante Castelán Carpinteyro; su
                                         programador.</i></span></em></strong></p>
                 </div>
