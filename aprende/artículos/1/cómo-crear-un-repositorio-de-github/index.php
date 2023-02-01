bf523ffb1ccbf6e273a21f91e3c7afbfb5a0641e
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-01-31 22:00:33 -0600

    ..

--- aprende/artículos/1/cómo-crear-un-repositorio-de-github/index.php
+++ aprende/artículos/1/cómo-crear-un-repositorio-de-github/index.php
@@ -37,6 +37,8 @@
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/Carousel-Hero.css">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/extra.css">
     <link rel="stylesheet" href="https://castelancarpinteyro.com/assets/css/uiverse.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 
 <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
