a8a336b3c945ce1c39686828dbcc16815d1696cb
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-07 23:15:55 -0600

    comentario temporal

--- php scripts/proto-login.php
+++ php scripts/proto-login.php
@@ -14,22 +14,22 @@ if ($conexiónPDO->connect_error) {
 }
 
 // Procesar el input de usuario y contraseña
-$username = mysqli_real_escape_string($conexiónPDO, $_POST['username']);
-$password = mysqli_real_escape_string($conexiónPDO, $_POST['password']);
-
-// Buscar el usuario y contraseña en la tabla de usuarios
-$sql = "SELECT * FROM usuarios WHERE nombre_de_usuario = '$username' AND contraseña = '$password'";
-$resultado = $conexiónPDO->query($sql);
-
-// Verificar si se encontró un usuario válido
-if ($resultado->num_rows > 0) {
-    // Acceso concedido, redireccionar a la página de inicio del sitio web
-    header("location: index.php");
-} else {
-    // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
-    echo "Nombre de usuario o contraseña incorrectos";
-    header("location: login.php");
-}
+#$username = mysqli_real_escape_string($conexiónPDO, $_POST['username']);
+#$password = mysqli_real_escape_string($conexiónPDO, $_POST['password']);
+#
+#// Buscar el usuario y contraseña en la tabla de usuarios
+#$sql = "SELECT * FROM usuarios WHERE nombre_de_usuario = '$username' AND contraseña = '$password'";
+#$resultado = $conexiónPDO->query($sql);
+#
+#// Verificar si se encontró un usuario válido
+#if ($resultado->num_rows > 0) {
+#    // Acceso concedido, redireccionar a la página de inicio del sitio web
+#    header("location: index.php");
+#} else {
+#    // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
+#    echo "Nombre de usuario o contraseña incorrectos";
+#    header("location: login.php");
+#}
 
 // Cerrar la conexión a la base de datos
 $conexiónPDO->close();
