d87eb948aca54d8546bda9b74eb901f49bde224d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-07 23:14:48 -0600

    CONEXIÓN PDO creada

--- php scripts/Conexión.php
+++ php scripts/Conexión.php
@@ -1,2 +1,6 @@
 <?php
 $conexión = mysqli_connect("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
+
+$conexión[1] = mysqli_connect("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
+$conexión[2] = new mysqli("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
+$conexiónPDO = $conexión[2];
--- php scripts/proto-login.php
+++ php scripts/proto-login.php
@@ -1,34 +1,35 @@
 <?php
 // Configurar la conexión a la base de datos
-$servername = "nombre_del_servidor";
-$username = "nombre_de_usuario";
-$password = "contraseña";
-$dbname = "nombre_de_la_base_de_datos";
-$conn = new mysqli($servername, $username, $password, $dbname);
+#$servername = "nombre_del_servidor";
+#$username = "nombre_de_usuario";
+#$password = "contraseña";
+#$dbname = "nombre_de_la_base_de_datos";
+#$conexiónPDO = new mysqli($servername, $username, $password, $dbname);
+
+include "Conexión.php";
 
 // Verificar si la conexión es exitosa
-if ($conn->connect_error) {
-  die("La conexión a la base de datos falló: " . $conn->connect_error);
+if ($conexiónPDO->connect_error) {
+    die("La conexión a la base de datos falló: " . $conexiónPDO->connect_error);
 }
 
 // Procesar el input de usuario y contraseña
-$username = mysqli_real_escape_string($conn, $_POST['username']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
+$username = mysqli_real_escape_string($conexiónPDO, $_POST['username']);
+$password = mysqli_real_escape_string($conexiónPDO, $_POST['password']);
 
 // Buscar el usuario y contraseña en la tabla de usuarios
 $sql = "SELECT * FROM usuarios WHERE nombre_de_usuario = '$username' AND contraseña = '$password'";
-$resultado = $conn->query($sql);
+$resultado = $conexiónPDO->query($sql);
 
 // Verificar si se encontró un usuario válido
 if ($resultado->num_rows > 0) {
-  // Acceso concedido, redireccionar a la página de inicio del sitio web
-  header("location: index.php");
+    // Acceso concedido, redireccionar a la página de inicio del sitio web
+    header("location: index.php");
 } else {
-  // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
-  echo "Nombre de usuario o contraseña incorrectos";
-  header("location: login.php");
+    // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
+    echo "Nombre de usuario o contraseña incorrectos";
+    header("location: login.php");
 }
 
 // Cerrar la conexión a la base de datos
-$conn->close();
-?>
+$conexiónPDO->close();
