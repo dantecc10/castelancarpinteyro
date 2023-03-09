48e5e5a53c88ce4b0e326f3c02955d505c7a1fd5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-08 23:50:06 -0600

    login

--- php scripts/login.php
+++ php scripts/login.php
@@ -6,5 +6,22 @@ if ($conexiónPDO->connect_error) {
 } else {
     echo ("Conexión establecida");
 }
+
 $username = mysqli_real_escape_string($conexiónPDO, $_POST['username']);
 $password = mysqli_real_escape_string($conexiónPDO, $_POST['password']);
+
+$sql = "SELECT * FROM usuarios WHERE (`email_usuario` = '$username' OR `email_dominio` = '$username') AND `password_usuario` = '$password'";
+$resultado = $conexiónPDO->query($sql);
+
+// Verificar si se encontró un usuario válido
+if ($resultado->num_rows > 0) {
+    // Acceso concedido, redireccionar a la página de inicio del sitio web
+    header("location: index.php");
+} else {
+    // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
+    echo "Nombre de usuario o contraseña incorrectos";
+    header("location: login.php");
+}
+
+// Cerrar la conexión a la base de datos
+$conexiónPDO->close();
