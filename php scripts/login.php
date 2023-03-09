4ec518bfa2fdd37c9d3d01c6f70f9af89ae57c56
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-08 23:46:53 -0600

    comentando

--- php scripts/login.php
+++ php scripts/login.php
@@ -1,11 +1,11 @@
 <?php
 include "Conexión.php";
 
-if ($conexiónPDO->connect_error) {
-    die("La conexión a la base de datos falló: " . $conexiónPDO->connect_error);
-} else {
-    echo ("Conexión establecida");
-}
+#if ($conexiónPDO->connect_error) {
+#    die("La conexión a la base de datos falló: " . $conexiónPDO->connect_error);
+#} else {
+#    echo ("Conexión establecida");
+#}
 
 #$username = mysqli_real_escape_string($conexiónPDO, $_POST['username']);
 #$password = mysqli_real_escape_string($conexiónPDO, $_POST['password']);
