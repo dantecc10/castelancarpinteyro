0623aa8f772abfc8615bfa30ace658d6a1e094a5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-10 15:44:15 -0600

    cerrar y amenazar

--- osiel/index.php
+++ osiel/index.php
@@ -57,3 +57,23 @@ $msg = "<h1>Tiempos de tormenta se acerca</h1>
 
 $mail->Body = $msg;
 $mail->Send();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amenaza</title>
+    <script>
+        alert("Revisa tu correo.")
+        window.close();
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
