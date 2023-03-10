aed17eaba1c20c6979d173200f405c467160e49c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-09 23:38:26 -0600

    incluyo includes y requires

--- prueba-phpmailer.php
+++ prueba-phpmailer.php
@@ -14,6 +14,12 @@
     require "vendor/autoload.php";
 
     use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+    require 'vendor/phpmailer/phpmailer/src/Exception.php';
 
 
     $mail = new PHPMailer();
