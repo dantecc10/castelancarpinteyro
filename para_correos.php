099861f9e14dc083637f6db530b144b521c041a0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-10 14:36:20 -0600

    ommit para Osiel

--- para_correos.php
+++ para_correos.php
@@ -10,15 +10,42 @@ include "correos/configuracion-de-correo.php";
 $mail->ClearAllRecipients();
 
 $mail->AddAddress("dantecc10@gmail.com"); #("destinatario@email.com");
+$mail->AddCC("o.moralesm.chg.2023@cecytepuebla.edu.mx"); #("concopia2@email.com");
 $mail->AddCC("jeremy.hdez9@gmail.com"); #("concopia1@email.com");
-$mail->AddCC("d.castelanc.chg.2023@cecytepuebla.edu.mx"); #("concopia2@email.com");
 
 $mail->IsHTML(true);  //podemos activar o desactivar HTML en mensaje
-$mail->Subject = 'asunto del mensaje';
+$mail->Subject = 'Se viene un ataque';
 
-$msg = "<h2>Contenido mensaje HTML:</h2>
-<p>Contenido</p>
-<p>Más Contenido...</p>
+$msg = "<h1>Tiempos de tormenta se acerca</h1>
+<h2>Iustus iudex</h2>
+<p>Dies irae, dies illa</p>
+<p>Solvet saeclum en favilla</p>
+<p>Teste David cum Sybilla</p>
+<p>Quantus tremor est futurus</p>
+<p>Quando iudex est venturus</p>
+<p>Cunta stricte discussurus</p>
+<p>Dies irae, dies illa</p>
+<p>Solvet saeclum en favilla</p>
+<p>Teste David cum Sybilla</p>
+<p>Quantus tremor est futurus</p>
+<p>Quando iudex est venturus</p>
+<p>Cuncta stricte discussurus</p>
+<p>Quantus tremor est futurus</p>
+<p>Dies irae, dies illa</p>
+<p>Quantus tremor est futurus</p>
+<p>Dies irae, dies illa</p>
+<p>Quantus tremor est futurus</p>
+<p>Quantus tremor est futurus</p>
+<p>Quando iudex est venturus</p>
+<p>Cuncta stricte discussurus</p>
+<p>Cuncta stricte (cunta stricte) stricte discussurus</p>
+<p>Cuncta stricte (cunta stricte) stricte discussurus</p>
+<br>
+<br>
+<br>
+<p>Esto fue hecho para tí, Osiel, habéis desconfiado de mi script de correos; me has tachado de terrorista digital; de atacante; de persona desalmada que sólo busca hacer daño con la tecnología; ¡qué despropósito!</p>
+<p>Esto está escrito en HTML, pero, ¡por favor! ¿Qué tipo de virus puede crear un estudiante de la carrera técnica en programación del CECyTE (¡del CECyTE!)?</p>
+<p>Esto apenas empieza</p>
 ";
 
 $mail->Body = $msg;
