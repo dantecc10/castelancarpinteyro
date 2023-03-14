b376e579f4ff8e58aa2f42359a661e838dec65e5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-13 20:42:03 -0600

    regreso a las contraseñas puestas

--- php scripts/Conexión.php
+++ php scripts/Conexión.php
@@ -1,9 +1,7 @@
 <?php
-include "secrets.php";
-$contraseñaDB = $contraseñaDBcastelancarpinteyro;
 
-$conexión = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
+$conexión = mysqli_connect("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
 
-$conexión[1] = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
-$conexión[2] = new mysqli("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
-$conexiónPDO = new mysqli("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
+$conexión[1] = mysqli_connect("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
+$conexión[2] = new mysqli("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
+$conexiónPDO = new mysqli("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
