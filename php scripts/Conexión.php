1558b871d19eadd3813e01907f08741f4dbb0216
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-13 20:38:58 -0600

    variable de transición

--- php scripts/Conexión.php
+++ php scripts/Conexión.php
@@ -1,8 +1,9 @@
 <?php
 include "secrets.php";
+$contraseñaDB = $contraseñaDBcastelancarpinteyro;
 
-$conexión = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDBcastelancarpinteyro, "castelancarpinteyro");
+$conexión = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
 
-$conexión[1] = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDBcastelancarpinteyro, "castelancarpinteyro");
-$conexión[2] = new mysqli("localhost", "castelancarpinteyro", $contraseñaDBcastelancarpinteyro, "castelancarpinteyro");
-$conexiónPDO = new mysqli("localhost", "castelancarpinteyro", $contraseñaDBcastelancarpinteyro, "castelancarpinteyro");
+$conexión[1] = mysqli_connect("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
+$conexión[2] = new mysqli("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
+$conexiónPDO = new mysqli("localhost", "castelancarpinteyro", $contraseñaDB, "castelancarpinteyro");
