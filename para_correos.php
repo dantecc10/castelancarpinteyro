12a17bb4616bdf91190f05a3dba3f06e49dcbb68
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-17 10:29:23 -0600

    prototipo de correos

--- para_correos.php
+++ para_correos.php
@@ -7,16 +7,16 @@ include "vendor/autoload.php";
 
 include "correos/configuracion-de-correo.php";
 
+$asunto = $_GET['asunto'];
+$mensaje = $_GET['mensaje'];
+$destinatario = $_GET['destinatario'];
+
 $mail->ClearAllRecipients();
-
-$mail->AddAddress("dantecc10@gmail.com"); #("destinatario@email.com");
-$mail->AddCC("o.moralesm.chg.2023@cecytepuebla.edu.mx"); #("concopia2@email.com");
+$mail->AddAddress("$destinatario"); #("destinatario@email.com");
+$mail->AddCC("dante@castelancarpinteyro.com"); #("concopia2@email.com");
 $mail->AddCC("jeremy.hdez9@gmail.com"); #("concopia1@email.com");
-
 $mail->IsHTML(true);  //podemos activar o desactivar HTML en mensaje
-$mail->Subject = 'Se viene un ataque';
-
-$msg = "Hola";
-
+$mail->Subject = $asunto;
+$msg = $mensaje;
 $mail->Body = $msg;
 $mail->Send();
