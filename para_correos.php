63e2cae1dbcb1827a1afbd9f645375c04216c0cd
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-03-17 11:28:25 -0600

    a ver

--- para_correos.php
+++ para_correos.php
@@ -12,16 +12,16 @@ include "php scripts/secrets.php";
 #$mensaje = $_GET['mensaje'];
 #$destinatario = $_GET['destinatario'];
 
-$destinatario = 'dannapaolaordonezfaro@gmail.com';
+$destinatario = 'dantecc10@gmail.com';
 $asunto = 'Credenciales del servidor para el equipo 4.';
 $mensaje = 'Hola, Dana Paola, a través de este correo te envío las credenciales de acceso al servidor de tu equipo, el número 4. El usuario será prog4b4 y la contraseña es Prog4B4!!. Ingresa desde prog5a.com:8443. Atentamente, Dante Castelán Carpinteyro.';
 
 $mail->ClearAllRecipients();
-$mail->AddAddress("$destinatario"); #("destinatario@email.com");
-$mail->AddCC("dante@castelancarpinteyro.com"); #("concopia2@email.com");
-$mail->AddCC("jeremy.hdez9@gmail.com"); #("concopia1@email.com");
-$mail->IsHTML(true);  //podemos activar o desactivar HTML en mensaje
-$mail->Subject = "$asunto";
-$msg = "$mensaje";
+$mail->AddAddress($destinatario);
+$mail->AddCC('dante@castelancarpinteyro.com');
+$mail->AddCC('jeremy.hdez9@gmail.com');
+$mail->IsHTML(true);
+$mail->Subject = $asunto;
+$msg = $mensaje;
 $mail->Body = $msg;
 $mail->Send();
