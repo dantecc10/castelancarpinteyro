2a606110b6e68ecd4703803d09cf8e2ee570ea7e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-04-09 13:41:23 -0500

    prueba

--- pruebas/DeepSpeech.php
+++ pruebas/DeepSpeech.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+
 </head>
 
 <body>
@@ -174,6 +176,8 @@
         
         */
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
