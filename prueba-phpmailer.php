e7f2697de9966cbf0a6aa1d6a8cc1a817311763a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-05-13 22:13:51 -0600

    quito eso

--- prueba-phpmailer.php
+++ prueba-phpmailer.php
@@ -116,7 +116,7 @@
     $mail->SMTPAuth = true; // Cambiar a true si es necesario autenticar la conexión SMTP
     $mail->Port = 465; // Cambiar el puerto SMTP según la configuración del servidor
     $mail->Username = "dante@castelancarpinteyro.com";
-    $mail->Password = "DarkseidPower23!!";
+    $mail->Password = $contraseñaScripts;
 
     // Configuración del correo
     $mail->setFrom('dante@castelancarpinteyro.com', 'Dante CC');
