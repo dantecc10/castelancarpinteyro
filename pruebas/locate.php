f2188881c97c2b9ad84a352c5c1208bbc614fd65
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-16 08:44:45 -0600

    llamada

--- pruebas/locate.php
+++ pruebas/locate.php
@@ -13,10 +13,42 @@ echo $ip;
         var ip = "<?php echo $ip; ?>";
 
         function locate() {
+            var latitude;
+            var longitude;
+
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition);
             }
+
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            if (latitude != "" || latitude != null) {
+                var urlCompuesta, urlVariables = ("?ip=" + ip + "&latitude=" + latitude + "&longitude=" + longitude),
+                    uriPHP = "manageLocation.php";
+                urlCompuesta = (uriPHP + urlVariables);
+
+                //Petición AJAX
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        //document.getElementById(spanObjetivo).innerHTML = this.responseText;
+                        var mensaje = this.responseText;
+                    }
+                };
+
+                //Procesamiento AJAX
+                xmlhttp.open("GET", urlCompuesta, true);
+                console.log("URL: " + urlCompuesta + "\nURL Variables: " + urlVariables);
+                //console.log("ModoFiltro: " + ModoFiltro);
+                xmlhttp.send();
+
+            } else {
+                mensaje = "¡Error en el AJAX!";
+            }
         }
+
+        locate();
 
         function showError(error) {
             switch (error.code) {
@@ -26,33 +58,6 @@ echo $ip;
                 default:
                     break;
             }
-        }
-
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-
-        if (latitude != "" || latitude != null) {
-            var urlCompuesta, urlVariables = ("?ip=" + ip + "&latitude=" + latitude + "&longitude=" + longitude),
-                uriPHP = "manageLocation.php";
-            urlCompuesta = (uriPHP + urlVariables);
-
-            //Petición AJAX
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    //document.getElementById(spanObjetivo).innerHTML = this.responseText;
-                    var mensaje = this.responseText;
-                }
-            };
-
-            //Procesamiento AJAX
-            xmlhttp.open("GET", urlCompuesta, true);
-            console.log("URL: " + urlCompuesta + "\nURL Variables: " + urlVariables);
-            //console.log("ModoFiltro: " + ModoFiltro);
-            xmlhttp.send();
-
-        } else {
-            mensaje = "¡Error en el AJAX!";
         }
 
 
