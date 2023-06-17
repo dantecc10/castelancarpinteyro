f6d5a41af7e6e07eb13d2aa28312047e6f19413e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-17 01:17:59 -0600

    sin pedir click

--- pruebas/location/script.js
+++ pruebas/location/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('#locationLink').click(function(event) {
       event.preventDefault(); // Evita que el enlace redireccione
   
@@ -24,4 +24,23 @@ $(document).ready(function() {
       }
     });
   });
-  
+  */
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        // Enviar las coordenadas al servidor mediante AJAX
+        $.ajax({
+            type: 'POST',
+            url: 'save_location.php',
+            data: { latitude: latitude, longitude: longitude },
+            success: function (response) {
+                alert('Ubicación enviada correctamente.');
+            },
+            error: function () {
+                alert('Ha ocurrido un error al enviar la ubicación.');
+            }
+        });
+    });
+}
