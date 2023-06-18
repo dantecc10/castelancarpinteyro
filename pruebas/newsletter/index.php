2170ab8473bab47f18de3eecdd89ca85a5cff3d1
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-18 12:58:55 -0600

    sin try catch

--- pruebas/newsletter/index.php
+++ pruebas/newsletter/index.php
@@ -73,17 +73,18 @@ if ($result->num_rows > 0) {
         ";
 
         $mail->Body = $msg;
-        try {
-            // ...
-            // Código para enviar el correo electrónico
-            // ...
-
-            $mail->Send();
-            // Resto del código...
-        } catch (Exception $e) {
-            echo "Error al enviar el correo electrónico: " . $mail->ErrorInfo;
-            echo "Excepción lanzada: " . $e->getMessage();
-        }
+        $mail->Send();
+        //try {
+        //    // ...
+        //    // Código para enviar el correo electrónico
+        //    // ...
+//
+        //    $mail->Send();
+        //    // Resto del código...
+        //} catch (Exception $e) {
+        //    echo "Error al enviar el correo electrónico: " . $mail->ErrorInfo;
+        //    echo "Excepción lanzada: " . $e->getMessage();
+        //}
         //actualizar($id, $conn);*/
         // Actualizar el estado a "Enviado"
         $sql = "UPDATE `test_mn` SET status_mn = 'Enviado' WHERE id_mn = $id";
