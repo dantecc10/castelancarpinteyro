62a55ee8deee5d427454a2e1b980750f0030c1a7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-17 21:58:50 -0600

    mich actualizado

--- pruebas/newsletter/mich.php
+++ pruebas/newsletter/mich.php
@@ -53,7 +53,7 @@ if ($result->num_rows > 0) {
         $id = $row['id_mn'];
         //saludar($id);
 
-        include "https://castelancarpinteyro.com/php%20scripts/dynamicMailSettings.php";
+        include "../../php%20scripts/dynamicMailSettings.php";
 
         setMailParameters('newsletter', $mail);
         //use function PHPMailer\PHPMailer\setMailParameters;
