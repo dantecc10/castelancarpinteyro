9aa50c7e21a55075f97e81548636282ce38536ae
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-17 19:40:35 -0600

    cargo dynamicMailSettings con dynamicSecrets listo

---
+++ pruebas/newsletter/test_mn.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `test_mn` (
+    `id_mn` INT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `contenido_mn` VARCHAR(255) NOT NULL,
+    `nombre_destino_mn` VARCHAR(255) NOT NULL,
+    `email_destino_mn` VARCHAR(255) NOT NULL,
+    `status_mn` VARCHAR(255) NOT NULL,
+    `fecha_mn` DATE NOT NULL
+);
