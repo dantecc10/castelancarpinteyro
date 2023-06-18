fff856c807a6a63ba56aefeda1d3fdae181d4a46
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-17 22:20:40 -0600

    incluyo respaldo para ver qué tal

--- pruebas/newsletter/mich.php
+++ pruebas/newsletter/mich.php
@@ -53,50 +53,8 @@ if ($result->num_rows > 0) {
         $id = $row['id_mn'];
         //saludar($id);
 
-        //include "../../php%20scripts/respaldoDynamicMailSettings.php";
-        //---------
-        //require_once('phpmailer/PHPMailerAutoload.php');
-//la version con vulnerabilidades es la linea justo debajo
-//require_once('PHPMailerAutoload.php');
-//nueva version 6.1.1
+        include "../../php%20scripts/respaldoDynamicMailSettings.php";
 
-namespace PHPMailer\PHPMailer;
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
-
-require_once('../../vendor/phpmailer/phpmailer/src/PHPMailer.php');
-require_once('../../vendor/phpmailer/phpmailer/src/SMTP.php');
-require_once('../../vendor/phpmailer/phpmailer/src/Exception.php');
-
-
-//$mail->SMTPDebug    = 3;
-$mail = new PHPMailer;
-$mail->IsSMTP();
-$mail->Host = "castelancarpinteyro.com"; #'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
-$mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
-$mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
-$mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
-$mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
-
-
-include "../../php scripts/dynamicSecrets.php";
-$turing = "newsletter";
-$data = generatePasskey($turing);
-
-$mail->Username = $data[2]; #'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
-$mail->Password = $data[0];   /*Tu contraseña*/
-$mail->From = $data[2]; #'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
-$mail->FromName = $data[1];   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
-    //echo ($data[0] . " " . $data[1] . " " . $data[2]); // Debug command line
-
-//setMailParameters('dante', $mail);
-//echo $mail;
-        //---------
-        
-        //setMailParameters('newsletter', $mail);
-        //use function PHPMailer\PHPMailer\setMailParameters;
 
         $mail->ClearAllRecipients();
 
