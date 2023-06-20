7554343b161f8375b39ba8f5aff515ee5b70c973
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-19 22:22:24 -0600

    comento ajustes pre-envío

--- pruebas/newsletter/envío.php
+++ pruebas/newsletter/envío.php
@@ -24,23 +24,23 @@ setMailParameters('newsletter');
 for ($i = 0; $i < $_SESSION['límite']; $i++) {
     if ($_SESSION['id'][$i] != null) {
 
-        $mail->ClearAllRecipients();
-
-        $mail->AddAddress($_SESSION['email'][$i]);
-        $mail->AddCC("dante@castelancarpinteyro.com");
-        $mail->AddCC("dantecc10@gmail.com");
-
-        $mail->IsHTML(true);  //podemos activar o desactivar HTML en mensaje
-        $mail->Subject = 'Correo de prueba del newsletter de Castelán Carpinteyro';
-
-        $msg = "<h1>¡Hola" . $_SESSION['nombre'][$i] . "</h1>
-            <p>Según la base de datos, hoy " . $_SESSION['fecha'][$i] . " hay un mensaje para tí desde el newsletter</p>
-            <p>De parte de <b><i>Dante Castelán Carpinteyro</i></b>, recibe el siguiente mensaje: '" . $_SESSION['mensaje'][$i] . "'.</p>
-            <p>¡Gracias por ser parte de mis pruebas en el servidor! Me ayudas mucho. Por favor, siéntete libre de responder a este correo o por el medio que desees más mensajes personalizados para que los programe.</p>
-        ";
-
-        $mail->Body = $msg;
-        $mail->Send();
+        //$mail->ClearAllRecipients();
+//
+        //$mail->AddAddress($_SESSION['email'][$i]);
+        //$mail->AddCC("dante@castelancarpinteyro.com");
+        //$mail->AddCC("dantecc10@gmail.com");
+//
+        //$mail->IsHTML(true);  //podemos activar o desactivar HTML en mensaje
+        //$mail->Subject = 'Correo de prueba del newsletter de Castelán Carpinteyro';
+//
+        //$msg = "<h1>¡Hola" . $_SESSION['nombre'][$i] . "</h1>
+        //    <p>Según la base de datos, hoy " . $_SESSION['fecha'][$i] . " hay un mensaje para tí desde el newsletter</p>
+        //    <p>De parte de <b><i>Dante Castelán Carpinteyro</i></b>, recibe el siguiente mensaje: '" . $_SESSION['mensaje'][$i] . "'.</p>
+        //    <p>¡Gracias por ser parte de mis pruebas en el servidor! Me ayudas mucho. Por favor, siéntete libre de responder a este correo o por el medio que desees más mensajes personalizados para que los programe.</p>
+        //";
+//
+        //$mail->Body = $msg;
+        //$mail->Send();
 
         //try {
         //    // ...
