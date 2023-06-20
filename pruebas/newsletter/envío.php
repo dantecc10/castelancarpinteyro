a64d3a8fbe19c4fb8e1b4da15661f3ab26ef0546
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-06-19 23:27:56 -0600

    VACIANDO VARIABLES DE SESIÓN

--- pruebas/newsletter/envío.php
+++ pruebas/newsletter/envío.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace PHPMailer\PHPMailer;
+
 session_start();
 
 //include "../../php scripts/dynamicMailSettings.php"; // Importación de configuración dinámica
@@ -14,25 +15,25 @@ require_once('../../vendor/phpmailer/phpmailer/src/PHPMailer.php');
 require_once('../../vendor/phpmailer/phpmailer/src/SMTP.php');
 require_once('../../vendor/phpmailer/phpmailer/src/Exception.php');
 
-    include "../../php scripts/dynamicSecrets.php";
-    include "../../php scripts/secrets.php.php";
+include "../../php scripts/dynamicSecrets.php";
+include "../../php scripts/secrets.php.php";
 
-    $data = generatePasskey('newsletter');
+$data = generatePasskey('newsletter');
 
-    //$mail->SMTPDebug    = 3;
-    $mail = new PHPMailer;
-    $mail->IsSMTP();
-    $mail->Host = "castelancarpinteyro.com"; #'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
-    $mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
-    $mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
-    $mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
-    $mail->Username = $data[2]; #'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
-    $mail->Password = $data[0];   /*Tu contraseña*/
-    $mail->From = $data[2]; #'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
-    $mail->FromName = $data[1];   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
-    $mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
-    //echo ($data[0] . " " . $data[1] . " " . $data[2]); // Debug command line
-    
+//$mail->SMTPDebug    = 3;
+$mail = new PHPMailer;
+$mail->IsSMTP();
+$mail->Host = "castelancarpinteyro.com"; #'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
+$mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
+$mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
+$mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
+$mail->Username = $data[2]; #'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
+$mail->Password = $data[0];   /*Tu contraseña*/
+$mail->From = $data[2]; #'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
+$mail->FromName = $data[1];   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
+$mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
+//echo ($data[0] . " " . $data[1] . " " . $data[2]); // Debug command line
+
 
 //$mail = setMailParameters('dante');
 //echo $mail;
@@ -67,3 +68,37 @@ for ($i = 0; $i < $_SESSION['límite']; $i++) {
         }
     }
 }
+
+// Eliminar todos los elementos de $_SESSION['id']
+foreach ($_SESSION['id'] as $index => $value) {
+    unset($_SESSION['id'][$index]);
+}
+
+$_SESSION['id'] = array();
+
+
+foreach ($_SESSION['nombre'] as $index => $value) {
+    unset($_SESSION['nombre'][$index]);
+}
+
+$_SESSION['nombre'] = array();
+
+
+foreach ($_SESSION['email'] as $index => $value) {
+    unset($_SESSION['email'][$index]);
+}
+
+$_SESSION['email'] = array();
+
+
+foreach ($_SESSION['fecha'] as $index => $value) {
+    unset($_SESSION['fecha'][$index]);
+}
+
+$_SESSION['fecha'] = array();
+
+
+foreach ($_SESSION['mensaje'] as $index => $value) {
+    unset($_SESSION['mensaje'][$index]);
+}
+$_SESSION['mensaje'] = array();
