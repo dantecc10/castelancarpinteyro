8a12f372831ee2b9a7ff705f2af2d67bc8dcf39f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-07-09 09:34:33 -0600

    a ver le checo

--- php scripts/signin.php
+++ php scripts/signin.php
@@ -1,2 +1,37 @@
 <?php
 //Script para procesar formulario de inicio de sesión
+include 'php scripts/Conexión.php';
+
+// Configuración de la conexión a la base de datos
+//$servername = "localhost";
+//$username = "nombre_usuario";
+//$password = "contraseña";
+//$dbname = "nombre_base_de_datos";
+
+// Datos del formulario de registro
+$email = $_POST['email'];
+
+// Crear la conexión
+//$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conexiónPDO->connect_error) {
+    die("Conexión fallida: " . $conexiónPDO->connect_error);
+}
+
+// Consulta para verificar si el usuario ya existe
+$sql = "SELECT * FROM `usuarios` WHERE `email_usuario` = '$email'";
+$result = $conexiónPDO->query($sql);
+
+// Verificar si se encontraron resultados
+if ($result->num_rows > 0) {
+    // El usuario ya existe, mostrar un mensaje de error o realizar alguna acción adicional
+    echo "El usuario ya está registrado.";
+} else {
+    // El usuario no existe, se puede proceder con el registro
+    // Aquí puedes incluir el código para insertar los datos del nuevo usuario en la base de datos
+    echo "Registro exitoso.";
+}
+
+// Cerrar la conexión
+$conexiónPDO->close();
