94523beffd186d276e56c2a1ded84947b87924ec
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-07-19 00:10:28 -0600

    orden

---
+++ Otros/correos/configuracion-de-correo.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+*
+* Están los archivos metidos así, manual. Me gustaría que me enseñaras a usar el composer porque nomás no lo he hecho, sino que manualmente voy metiendo las cosas. Pero sirve bien este
+* 
+*
+*/
+
+//require_once('phpmailer/PHPMailerAutoload.php');
+//la version con vulnerabilidades es la linea justo debajo
+//require_once('PHPMailerAutoload.php');
+//nueva version 6.1.1
+namespace PHPMailer\PHPMailer;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+require_once('PHPMailer.php');
+require_once('SMTP.php');
+require_once('Exception.php');
+
+include "../php scripts/secrets.php";
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug    = 3;
+
+$mail->IsSMTP();
+$mail->Host = "castelancarpinteyro.com";#'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
+$mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
+$mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
+$mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
+$mail->Username = "contacto@castelancarpinteyro.com";#'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
+$mail->Password = $contraseñaScripts;   /*Tu contraseña*/
+$mail->From = "contacto@castelancarpinteyro.com";#'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
+$mail->FromName = 'Castelán Carpinteyro';   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
+$mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
---
+++ Otros/includes/consola-linux.php
@@ -0,0 +1,19 @@
+<div class="contenedor">
+    <div class="container_terminal"></div>
+    <div class="terminal_toolbar">
+        <div class="butt">
+            <button class="btn-terminal btn-color-terminal"></button>
+            <button class="btn-terminal"></button>
+            <button class="btn-terminal"></button>
+        </div>
+        <p class="user-terminal">dantedev@admin: ~</p>
+    </div>
+    <div class="terminal_body">
+        <div class="terminal_promt">
+            <span class="terminal_user">dantedev@admin:</span>
+            <span class="terminal_location">~</span>
+            <span class="terminal_bling">$</span>
+            <span class="terminal_cursor"></span>
+        </div>
+    </div>
+</div>
