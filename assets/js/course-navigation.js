269506bde7a3f4bf2f0383afed2589e25c810662
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-07-21 23:51:13 -0600

    avance en js

---
+++ assets/js/course-navigation.js
@@ -0,0 +1,46 @@
+function courseNavigate(lesson) {
+    var lastParameterCharacter = "";
+
+    // Variable tipo string que permite dinamizar el contenido visible y estilizar los índices
+    /*
+    Formato de clases (barra lateral):
+    - lateralP
+    - lateralT
+    - lateralN (N es el número de lección)
+    - lateralE
+    - lateralC
+
+    Formato de clases (cinta de navegación):
+    - cintaP
+    - cintaT
+    - cintaN (N es el número de lección)
+    - cintaE
+    - cintaC
+    */
+    //lastParameterCharacter es el último caracter en lesson
+    //Saber si lesson.charAt(length(lesson) - 1) es un número
+    lastParameterCharacter = lesson.charAt((lesson.length) - 1); // Operative ✅
+
+    if (parseInt(lastParameterCharacter)) {
+        console.log(lastParameterCharacter);
+    } else {
+        console.log("No es un número"); // Debug 🐞
+        switch (lastParameterCharacter) {
+            case 'P':
+                console.log("Se hace referencia a la presentación."); // Debug 🐞
+                break;
+            case 'T':
+                console.log("Se hace referencia al temario."); // Debug 🐞
+                break;
+            case 'E':
+                console.log("Se hace referencia a la evaluación."); // Debug 🐞
+                break;
+            case 'C':
+                console.log("Se hace referencia al certificado."); // Debug 🐞
+                break;
+
+            default:
+                console.log("No se ha encontrado ninguna opción."); // Debug 🐞
+        }
+    }
+}
