8d334028127ad52376a27fb1be18dd8f82d4121c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-07-26 15:00:01 -0600

    var name

--- php scripts/suscripción.php
+++ php scripts/suscripción.php
@@ -8,7 +8,7 @@ $email = $_POST['email'];
 //Create a row in the `newsletter_subscribers` table; that has the following fields: `id_subscriber` (has auto_increment), `email_subscriber` (must be cleaned as it's an user input), and `status_subscriber`, set on 1. Don't forget to use stmt and real_escape_string fr the email
 $stmt = $db->prepare("INSERT INTO `newsletter_subscribers` VALUES ('', ?, 1)");
 //continue
-$stmt->bind_param("s", $email);
+$stmt->bind_param("s", $cleanEmail);
 $cleanEmail = $db->real_escape_string($email);
 $stmt->execute();
 $stmt->close();
