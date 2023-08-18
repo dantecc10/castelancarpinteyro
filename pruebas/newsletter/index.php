202dcec83042dd62d3f688efda16440617dde19a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-08-17 20:31:49 -0600

    Updated database connection credentials.

--- pruebas/newsletter/index.php
+++ pruebas/newsletter/index.php
@@ -6,7 +6,7 @@ function send()
 }
 
 // Conexión a la base de datos
-$conn = new mysqli("localhost", "darkseid", "DarkseidPower23!!", "castelancarpinteyro");
+$conn = new mysqli("localhost", "castelancarpinteyro", "@CastelanCarpinteyroWEB", "castelancarpinteyro");
 
 // Verificar la conexión
 if ($conn->connect_error) {
