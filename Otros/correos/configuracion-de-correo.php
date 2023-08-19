0d2d3cf68a087fa2577b96a09aac0a97a137691d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-08-19 11:31:35 -0600

    debug on

--- Otros/correos/configuracion-de-correo.php
+++ Otros/correos/configuracion-de-correo.php
@@ -16,6 +16,7 @@ namespace PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+
 require_once('PHPMailer.php');
 require_once('SMTP.php');
 require_once('Exception.php');
@@ -24,15 +25,15 @@ include "../php scripts/secrets.php";
 
 $mail = new PHPMailer;
 
-//$mail->SMTPDebug    = 3;
+$mail->SMTPDebug = 3;
 
 $mail->IsSMTP();
-$mail->Host = "castelancarpinteyro.com";#'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
+$mail->Host = "castelancarpinteyro.com"; #'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
 $mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
 $mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
 $mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
-$mail->Username = "contacto@castelancarpinteyro.com";#'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
+$mail->Username = "contacto@castelancarpinteyro.com"; #'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
 $mail->Password = $contraseñaScripts;   /*Tu contraseña*/
-$mail->From = "contacto@castelancarpinteyro.com";#'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
+$mail->From = "contacto@castelancarpinteyro.com"; #'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
 $mail->FromName = 'Castelán Carpinteyro';   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
 $mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
