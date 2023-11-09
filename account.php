27696d887ead5d9252a1ace4f2867eb3391ab80d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-09 17:11:03 -0600

    reparación de inhabilitado de input file

--- account.php
+++ account.php
@@ -147,6 +147,23 @@ if (!isset($_SESSION["id"])) {
 
                                         -->
                                         <style>
+                                            .input-label {
+                                                position: relative;
+                                                width: 80%;
+                                                height: 80%;
+                                                aspect-ratio: 1 / 1;
+                                                border-radius: 50%;
+                                                border: 2px solid rgb(1, 235, 252);
+                                                display: flex;
+                                                justify-content: center;
+                                                align-items: center;
+                                                overflow: hidden;
+                                                box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                animation: flicker 2s linear infinite;
+                                                cursor: pointer;
+                                                /* Agrega este estilo para que el cursor cambie al hacer clic en la etiqueta */
+                                            }
+
                                             div#centrador {
                                                 width: 100%;
                                                 display: flex;
@@ -245,13 +262,15 @@ if (!isset($_SESSION["id"])) {
                                         </style>
                                         <div id="centrador">
                                             <div class="input-div">
-                                                <input class="input" name="Ícono" type="file">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
-                                                    <polyline points="16 16 12 12 8 16"></polyline>
-                                                    <line y2="21" x2="12" y1="12" x1="12"></line>
-                                                    <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                                                    <polyline points="16 16 12 12 8 16"></polyline>
-                                                </svg>
+                                                <label for="archivoInput" class="input-label">
+                                                    <input id="archivoInput" class="input" name="Ícono" type="file">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                                        <polyline points="16 16 12 12 8 16"></polyline>
+                                                        <line y2="21" x2="12" y1="12" x1="12"></line>
+                                                        <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                                        <polyline points="16 16 12 12 8 16"></polyline>
+                                                    </svg>
+                                                </label>
                                             </div>
                                         </div>
                                         <input type="text" hidden name="src" id="src_input" value="<?php echo $_SESSION['img']; ?>">
