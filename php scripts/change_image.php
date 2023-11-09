34faabc2200aa1f936f03f1df6749780abdeb144
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-08 21:52:05 -0600

    trato de cambiar imagen

---
+++ php scripts/change_image.php
@@ -0,0 +1,30 @@
+<?php
+
+$file = $_FILES["Ícono"]["name"];
+$lastImg = $_POST['src'];
+
+$url_temp = $_FILES["Ícono"]["tmp_name"]; //Ruta temporal a donde se carga el archivo 
+
+//dirname(__FILE__) nos otorga la ruta absoluta hasta el archivo en ejecución
+$url_insert = (dirname(__FILE__, 1) . "/assets/img/avatar-icons/"); //Carpeta donde subiremos nuestros archivos
+
+//Ruta donde se guardara el archivo, usamos str_replace para reemplazar los "\" por "/"
+$url_target = str_replace('\\', '/', $url_insert) . '/' . $file;
+
+//Si la carpeta no existe, la creamos
+if (!file_exists($url_insert)) {
+    mkdir($url_insert, 0777, true);
+};
+
+//movemos el archivo de la carpeta temporal a la carpeta objetivo y verificamos si fue exitoso
+if (move_uploaded_file($url_temp, $url_target)) {
+    echo "El archivo " . htmlspecialchars(basename($file)) . " ha sido cargado con éxito.";
+} else {
+    echo "Ha habido un error al cargar tu archivo.";
+}
+
+$url_target = str_replace('\\', '/', $url_insert) . '/' . $lastImg;
+//Eliminar la imagen anterior
+unlink($url_target);
+
+header("Location: ../account.php");
