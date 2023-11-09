00c9f97be1eb98227f69d2c14d59700f98fc307f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-09 16:28:59 -0600

    carga de mejora estética de ícono

--- account.php
+++ account.php
@@ -94,7 +94,117 @@ if (!isset($_SESSION["id"])) {
                                 <div class="mb-3">
                                     <form action="php scripts/change_image.php" method="post" enctype="multipart/form-data">
                                         <label for="Ícono">Carga tu nueva imagen</label>
-                                        <input type="file" style="background-color: blue; color: white; border-radius: 10px;" accept="image/*" class="btn btn-sm" enabled name="Ícono" />
+                                        <!--
+                                            <input type="file" accept="image/*" enabled name="Ícono" />
+
+                                        -->
+                                        <style>
+                                            div#centrador {
+                                                width: 100%;
+                                                display: flex;
+                                                align-items: center;
+                                                justify-content: center;
+                                            }
+
+                                            .input-div {
+                                                position: relative;
+                                                width: 15%;
+                                                aspect-ratio: 1 / 1;
+                                                /*height: auto;*/
+                                                border-radius: 50%;
+                                                border: 2px solid rgb(1, 235, 252);
+                                                display: flex;
+                                                justify-content: center;
+                                                align-items: center;
+                                                overflow: hidden;
+                                                box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                animation: flicker 2s linear infinite;
+                                            }
+
+                                            .icon {
+                                                color: rgb(1, 235, 252);
+                                                font-size: 2rem;
+                                                cursor: pointer;
+                                                animation: iconflicker 2s linear infinite;
+                                            }
+
+                                            .input {
+                                                position: absolute;
+                                                opacity: 0;
+                                                width: 100%;
+                                                height: 100%;
+                                                cursor: pointer !important;
+                                            }
+
+                                            @keyframes flicker {
+                                                0% {
+                                                    border: 2px solid rgb(1, 235, 252);
+                                                    box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                }
+
+                                                5% {
+                                                    border: none;
+                                                    box-shadow: none;
+                                                }
+
+                                                10% {
+                                                    border: 2px solid rgb(1, 235, 252);
+                                                    box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                }
+
+                                                25% {
+                                                    border: none;
+                                                    box-shadow: none;
+                                                }
+
+                                                30% {
+                                                    border: 2px solid rgb(1, 235, 252);
+                                                    box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                }
+
+                                                100% {
+                                                    border: 2px solid rgb(1, 235, 252);
+                                                    box-shadow: 0px 0px 100px rgb(1, 235, 252), inset 0px 0px 10px rgb(1, 235, 252), 0px 0px 5px rgb(255, 255, 255);
+                                                }
+                                            }
+
+                                            @keyframes iconflicker {
+                                                0% {
+                                                    opacity: 1;
+                                                }
+
+                                                5% {
+                                                    opacity: 0.2;
+                                                }
+
+                                                10% {
+                                                    opacity: 1;
+                                                }
+
+                                                25% {
+                                                    opacity: 0.2;
+                                                }
+
+                                                30% {
+                                                    opacity: 1;
+                                                }
+
+                                                100% {
+                                                    opacity: 1;
+                                                }
+                                            }
+                                        </style>
+                                        <div id="centrador">
+                                            <div class="input-div">
+                                                <input class="input" name="Ícono" type="file">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon">
+                                                    <polyline points="16 16 12 12 8 16"></polyline>
+                                                    <line y2="21" x2="12" y1="12" x1="12"></line>
+                                                    <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                                                    <polyline points="16 16 12 12 8 16"></polyline>
+                                                </svg>
+                                            </div>
+                                        </div>
                                         <input type="text" hidden name="src" id="src_input" value="<?php echo $_SESSION['img']; ?>">
                                         <input type="submit" value="Cargar imagen" class="btn btn-primary">
                                     </form>
