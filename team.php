10c1ede8ad6e721b0081ede1ceef4a6913aa819d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-08 23:21:57 -0600

    teamp

--- team.php
+++ team.php
@@ -67,7 +67,54 @@
         </div>
     </nav><!-- End: Navbar Centered Links -->
     <section class="py-5 mt-5">
-        <!-- Start: Team -->
+        <div class="container py-5">
+            <div class="row mb-4 mb-lg-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <p class="fw-bold text-success mb-2">Soporte y desarrollo</p>
+                    <h2 class="fw-bold">Equipo</h2>
+                    <p class="text-muted w-lg-50">Este es el equipo que hace posible todo en <strong><em>Castelán Carpinteyro</em></strong>.</p>
+                </div>
+            </div>
+            <div class="row row-cols-2 row-cols-md-3 mx-auto" style="max-width: 700px;">
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar4.jpg">
+                        <h5 class="fw-bold mb-0"><strong>Dante Castelán Carpinteyro</strong></h5>
+                        <p class="text-muted mb-2">Desarrollador</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar6.jpg">
+                        <h5 class="fw-bold mb-0"><strong>Jeremy Hernández Balderas</strong></h5>
+                        <p class="text-muted mb-2">Desarrollador</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar5.jpg">
+                        <h5 class="fw-bold mb-0"><strong>Deiby Graciano Agudelo</strong></h5>
+                        <p class="text-muted mb-2">Desarrollador</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar3.jpg">
+                        <h5 class="fw-bold mb-0"><strong>John Smith</strong></h5>
+                        <p class="text-muted mb-2">Erat netus</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar1.jpg">
+                        <h5 class="fw-bold mb-0"><strong>John Smith</strong></h5>
+                        <p class="text-muted mb-2">Erat netus</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img class="rounded mb-3 fit-cover" width="150" height="150" src="../assets/img/team/avatar2.jpg">
+                        <h5 class="fw-bold mb-0"><strong>John Smith</strong></h5>
+                        <p class="text-muted mb-2">Erat netus</p>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End: Team -->
+        <!-- Start: Team 
         <div class="container py-5">
             <div class="row mb-5">
                 <div class="col-md-8 col-xl-6 text-center mx-auto">
@@ -111,7 +158,7 @@
                     </div>
                 </div>
             </div>
-        </div><!-- End: Team -->
+        </div> End: Team -->
     </section><!-- Start: Contact Details -->
     <section class="py-5">
         <div class="container">
