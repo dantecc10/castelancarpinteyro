48e67b4192f678c544342bebb0c28c4eacc23255
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-09 22:42:59 -0600

    ¿hay campos por actualizar

--- php scripts/save-account-changes.php
+++ php scripts/save-account-changes.php
@@ -2,17 +2,29 @@
 if (isset($_POST)) {
     #Trabajanding
 
+    $actualizar = [];
+
+    $datos = [["username", "usuario"], ["email", "email"], ["first_name", "nombre"], ["last_name1", "apellidoPaterno"], ["last_name2", "apellidoMaterno"], ["emailDominio", "emailDominio"], ["date", "nacimiento"]];
+
+    for ($i = 0; $i < sizeof($datos); $i++) {
+        # Agregar un elemento al arreglo en el último índice
+        if ($_POST[$datos[$i][0]] != $_SESSION[$datos[$i][1]]) {
+            array_push($actualizar, [$_POST[$datos[$i][0]], $i]);
+        }
+    }
+
+    if (sizeof($actualizar) > 0) {
+        echo ("Hay " . sizeof($actualizar) . " campo(s) por actualizar");
+    }
+
+
+
+    include "dynamicSecrets.php";
+    $data = generatePasskey('sql');
+
+    $db = new mysqli("localhost", $data[0], $data[1], $data[2]);
+
+
+    //header("Location: ../account.php?msg=success");
 
 }
-
-include "dynamicSecrets.php";
-$data = generatePasskey('sql');
-
-$db = new mysqli("localhost", $data[0], $data[1], $data[2]);
-
-if ($_POST["username"] != $_SESSION["usuario"]) {
-    # code...
-}
-
-
-header("Location: ../account.php?msg=success");
