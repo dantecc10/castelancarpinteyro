f1c2f7c0d1aab79d660f3a9a7d15764ddc207949
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-13 17:19:52 -0600

    Commit generado autom├íticamente por script

--- php scripts/build-chat.php
+++ php scripts/build-chat.php
@@ -28,26 +28,6 @@
                 echo $apMsgReceived . $_SESSION['content_msg'] . $clMsgReceived;
             }
         }
-        //fin replicando lógica de correos
-        /*
-        $objeto = $_SESSION['chat'];
-        // Verificar si el objeto es válido
-        if ($objeto) {
-            // Recorrer las propiedades del objeto
-            foreach ($objeto as $clave => $valor) {
-                echo "Mensaje:";
-                if ($valor->sender_msg == $_SESSION['id']) {
-                    // El mensaje fue enviado por el usuario logeado
-                    echo $apMsgSent . $valor->content_msg . $clMsgSent;
-                } else {
-                    // El mensaje fue enviado por el otro usuario
-                    echo $apMsgReceived . $valor->content_msg . $clMsgReceived;
-                }
-            }
-        } else {
-            echo "El objeto no está disponible en la sesión.";
-        }
-        */
     } else {
         echo "Aquí no hay nada de chats.";
     }
--- php scripts/sync-msg.php
+++ php scripts/sync-msg.php
@@ -1,16 +1,5 @@
 <?php
 session_start();
-function session_chat_store($a, $b, $c, $d, $e, $f, $index)
-{
-    array_push($_SESSION["chat"]["id_msg"], $a);
-    $_SESSION["chat"][$index] = array($a, $b, $c, $d, $e, $f);
-    $_SESSION["chat"]["id_msg"][$index] = $a;
-    $_SESSION["chat"]["sender_msg"][$index] = $b;
-    $_SESSION["chat"]["receiver_msg"][$index] = $c;
-    $_SESSION["chat"]["content_msg"][$index] = $d;
-    $_SESSION["chat"]["type_msg"][$index] = $e;
-    $_SESSION["chat"]["time_msg"][$index] = $f;
-}
 
 $otherUser = 5; // Constante para pruebas, luego dinámico para establecer el chateador
 $currentUser = $_SESSION['id'];
@@ -37,9 +26,6 @@ if ($stmt) {
     if ($resultado->num_rows > 0) {
         $i = 0;
         while ($fila = $resultado->fetch_assoc()) {
-            #$mensaje[$i]["user"] = $fila["receiver_msg"];
-            #$mensaje[$i]["texto"] = $fila["content_msg"];
-            #$mensaje[$i]["fecha"] = date("d/m/Y H:i:s", strtotime($fila["fecha"]));
             echo ("<br>" . var_dump($fila) . "<br>");
             $new_id_msg = ("" . $fila["id_msg"] . "");
             $new_receiver_msg = ("" . $fila["receiver_msg"] . "");
@@ -48,9 +34,8 @@ if ($stmt) {
             $new_type_msg = ("" . $fila["type_msg"] . "");
             $new_time_msg = ("" . $fila["time_msg"] . "");
 
-            //$_SESSION['chat'][$i] = [intval($new_id_msg), strval($new_sender_msg), strval($new_receiver_msg), strval($new_content_msg), strval($new_type_msg)];
+            $datos['chat'][$i] = [$new_id_msg, $new_receiver_msg, $new_sender_msg, $new_content_msg, $new_type_msg, $new_time_msg];
             $i++;
-            session_chat_store($new_id_msg, $new_sender_msg, $new_receiver_msg, $new_content_msg, $new_type_msg, $new_time_msg, $i);
         }
         $_SESSION['límite'] = $i;
         // Cerrar la conexión a la base de datos
