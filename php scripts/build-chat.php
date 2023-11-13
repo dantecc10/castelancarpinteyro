7986d24be9f9eba3b6c2ed58c98b1fb38b1a76b6
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-13 10:53:25 -0600

    upload new chat experimental files

---
+++ php scripts/build-chat.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de chat</title>
+    <link rel="stylesheet" href="msg.css">
+</head>
+
+<body>
+
+    <?php
+    session_start();
+    include "chat-configs.php";
+
+    if (isset($_SESSION['chat'])) {
+
+        $objeto = $_SESSION['chat'];
+        // Verificar si el objeto es válido
+        if ($objeto) {
+            // Recorrer las propiedades del objeto
+            foreach ($objeto as $clave => $valor) {
+                echo ("Mensaje");
+                if ($objeto->$sender_msg == $_SESSION['id']) {
+                    //El mensaje fue enviado por el usuario logeado
+                    echo ($apMsgSent . $objeto->content_msg . $clMsgSent);
+                } else {
+                    //El mensaje fue enviado por el otro usuario
+                    echo ($apMsgReceived . $objeto->content_msg . $clMsgReceived);
+                }
+            }
+        } else {
+            echo "El objeto no está disponible en la sesión.";
+        }
+    }
+    ?>
+</body>
+
+</html>
---
+++ php scripts/sync-msg.php
@@ -0,0 +1,51 @@
+<?php
+session_destroy();
+session_start();
+include "dynamicSecrets.php";
+$data = generatePasskey('sql');
+
+$conexiónPDO = new mysqli("localhost", $data[0], $data[1], $data[2]);
+
+if ($conexiónPDO->connect_error) {
+    die("La conexión a la base de datos falló: " . $conexiónPDO->connect_error);
+} else {
+    echo ("Conexión establecida");
+}
+$otherUser = 5;
+$user = mysqli_real_escape_string($conexiónPDO, $_SESSION['id']);
+$chatUser = mysqli_real_escape_string($conexiónPDO, $otherUser);
+
+#$username = mysqli_real_escape_string($conexiónPDO, $_POST['email']);
+#$password = mysqli_real_escape_string($conexiónPDO, $_POST['password']); //Recepción de variables que pasan por filtro anti explits SQL
+
+$sql = "SELECT * FROM `messages` WHERE (`receiver_msg` = '$currentUser' AND `sender_msg` = '$chatUser') OR (`sender_msg` = '$chatUser' AND `receiver_msg` = '$currentUser')";
+$resultado = $conexiónPDO->query($sql);
+
+// Verificar si se encontró un usuario válido
+if ($resultado->num_rows > 0) {
+    // Acceso concedido, redireccionar a la página de inicio del sitio web
+    if ($datos = $resultado->fetch_object()) { //Asignación y configuración de variables de sesión en arreglo de PHP
+        if(isset($_SESSION['chat'])){
+            if ($_SESSION['chat'] != $datos) {
+                // Hay mensajes nuevos
+                $_SESSION['chat'] = $datos;
+                echo "Añadimos mensajes."
+            } else {
+                // No hay nuevos mensajes
+                header("Location: build-chat.php");
+            }
+        }else{
+            $_SESSION['chat'] = $datos;
+        }
+    }
+    $conexiónPDO->close();
+    //$fecha=$_SESSION['nacimiento'];
+    header("Location: ../index.php");
+} else {
+    // Acceso denegado, mostrar un mensaje de error y redireccionar a la página de inicio de sesión
+    echo "No tienes un chat con el usuario " . $chatUser;
+    $conexiónPDO->close();
+    header("Location: ../account.php");
+}
+
+// Cerrar la conexión a la base de datos
