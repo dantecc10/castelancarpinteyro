dc0d018cdb94bbf435ce37ac787feac6d5542077
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-13 17:03:41 -0600

    texteando

--- php scripts/sync-msg.php
+++ php scripts/sync-msg.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
-function session_chat_store($a, $b, $c, $d, $e, $index)
+function session_chat_store($a, $b, $c, $d, $e, $f, $index)
 {
-    $_SESSION["chat"][$index] = array($a, $b, $c, $d, $e);
-    #$_SESSION["chat"]["id_msg"][$index] = $a;
-    #$_SESSION["chat"]["sender_msg"][$index] = $b;
-    #$_SESSION["chat"]["receiver_msg"][$index] = $c;
-    #$_SESSION["chat"]["content_msg"][$index] = $d;
-    #$_SESSION["chat"]["type_msg"][$index] = $e;
+    //$_SESSION["chat"][$index] = array($a, $b, $c, $d, $e);
+    $_SESSION["chat"]["id_msg"][$index] = $a;
+    $_SESSION["chat"]["sender_msg"][$index] = $b;
+    $_SESSION["chat"]["receiver_msg"][$index] = $c;
+    $_SESSION["chat"]["content_msg"][$index] = $d;
+    $_SESSION["chat"]["type_msg"][$index] = $e;
+    $_SESSION["chat"]["time_msg"][$index] = $f;
 }
 
 $otherUser = 5; // Constante para pruebas, luego dinámico para establecer el chateador
@@ -39,14 +40,15 @@ if ($stmt) {
             #$mensaje[$i]["texto"] = $fila["content_msg"];
             #$mensaje[$i]["fecha"] = date("d/m/Y H:i:s", strtotime($fila["fecha"]));
             echo ("<br>" . var_dump($fila) . "<br>");
-            $new_id_msg = $fila["id_msg"];
-            $new_sender_msg = $fila["sender_msg"];
-            $new_receiver_msg = $fila["receiver_msg"];
-            $new_content_msg = $fila["content_msg"];
-            $new_type_msg = $fila["type_msg"];
+            $new_id_msg = ("" . $fila["id_msg"] . "");
+            $new_receiver_msg = ("" . $fila["receiver_msg"] . "");
+            $new_sender_msg = ("" . $fila["sender_msg"] . "");
+            $new_content_msg = ("" . $fila["content_msg"] . "");
+            $new_type_msg = ("" . $fila["type_msg"] . "");
+            $new_time_msg = ("" . $fila["time_msg"] . "");
 
             //$_SESSION['chat'][$i] = [intval($new_id_msg), strval($new_sender_msg), strval($new_receiver_msg), strval($new_content_msg), strval($new_type_msg)];
-            session_chat_store($new_id_msg, $new_sender_msg, $new_receiver_msg, $new_content_msg, $new_type_msg, $i);
+            session_chat_store($new_id_msg, $new_sender_msg, $new_receiver_msg, $new_content_msg, $new_type_msg, $new_time_msg, $i);
 
             $i++;
         }
