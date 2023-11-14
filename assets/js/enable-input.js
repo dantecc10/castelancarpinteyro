2f86a6d9bda83c7af44c750292bae241a1f2a3e7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-13 19:35:56 -0600

    Commit generado autom├íticamente por script

--- assets/js/enable-input.js
+++ assets/js/enable-input.js
@@ -2,12 +2,5 @@ function activarInput(id) {
     // Obtener el elemento input por su ID
     var inputElement = document.getElementById(id);
     var status = inputElement.disabled;
-
-    if (status == false) {
-        // Habilitar el input
-        inputElement.disabled = false;
-    } else {
-        // Deshabilitar el input
-        inputElement.disabled = true;
-    }
+    inputElement.disabled = false;
 }
