d5f37ea33f9fc659d49698ef1573a1054b9e85dc
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-15 19:49:55 -0600

    Commit generado autom├íticamente por script

--- assets/js/chat-AJAX.js
+++ assets/js/chat-AJAX.js
@@ -1,18 +1,16 @@
 function rebuild() {
     // Obtener los valores de los campos de entrada
     document.getElementById('chat-section-container').innerHTML = "";
-
-    // Crear objeto XMLHttpRequest
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // Procesar la respuesta del servidor
-            document.getElementById('chat-section-container').innerHTML = this.response;
-        }
-    };
-    xhr.open("POST", "../../php scripts/build-chat.php", true);
-    //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    //xhr.send("receiver=" + receiver + "&content=" + content + "&type=" + type);
+    $(document).ready(function() {
+        $.ajax({
+            url: '../../php scripts/build-chat.php',
+            method: 'GET', // Método GET para enviar la solicitud sin variables
+            success: function(response) {
+                // Insertar la respuesta en el elemento con id="resultado"
+                $('#chat-section-container').html(response);
+            }
+        });
+    });
 }
 function addMsg() {
     // Obtener los valores de los campos de entrada
