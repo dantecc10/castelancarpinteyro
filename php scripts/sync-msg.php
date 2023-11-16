5220ad19871e1c447cec1e7acdff2044f26baa57
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-11-15 23:28:48 -0600

    Commit generado autom├íticamente por script

--- php scripts/sync-msg.php
+++ php scripts/sync-msg.php
@@ -20,7 +20,9 @@ if ($conexiónPDO->connect_error) {
         $stmt->bind_param("iiii", $currentUser, $chatUser, $currentUser, $chatUser);
         $stmt->execute();
         $resultado = $stmt->get_result();
-
+        if (isset($datos)) {
+            unset($datos);
+        }
         if ($resultado->num_rows > 0) {
             $i = 0;
             while ($fila = $resultado->fetch_assoc()) {
