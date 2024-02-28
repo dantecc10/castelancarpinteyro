9857a28408e1e68bdc1b25f1fb36da4b8246b6d5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-27 23:25:11 -0600

    try

--- php scripts/create-qr.php
+++ php scripts/create-qr.php
@@ -9,7 +9,7 @@ if (isset($_GET['url'])) {
     $qr_img = $qrcode->render($_GET['url']);
     $name = ("qr-" . time() . ".png");
     $path = ($name);
-    file_put_contents((__DIR__ . $path), $qr_img);
+    file_put_contents(("generated-qrs/".$path), $qr_img);
     header("Location: ../generated-qr.php?file=$name");
 } else {
     header("Location: ../qr-code.php");
