ad37916a18ae714bdea2ffc46bb8fad38f4d2903
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-27 23:42:45 -0600

    try

--- php scripts/create-qr.php
+++ php scripts/create-qr.php
@@ -1,16 +1,27 @@
 <?php
 
 use chillerlan\QRCode\QRCode;
-use chillerlan\QRCode\QROptions;
 
 if (isset($_GET['url'])) {
     include_once "../vendor/autoload.php";
+    
+    // Define el nombre del archivo del código QR
+    $name = "qr-" . time() . ".png";
+    
+    // Define la ruta del archivo del código QR
+    $path = __DIR__ . "/generated-qrs/" . $name;
+
+    // Crea una instancia de QRCode
     $qrcode = new QRCode;
-    $name = ("qr-" . time() . ".png");
-    $path = ($name);
-    $qr_img = $qrcode->render($_GET['url'], (__DIR__ . "/generated-qrs/" . $path));
-    //file_put_contents((__DIR__."generated-qrs/" . $path), $qr_img);
+
+    // Genera el código QR y guarda la imagen en la ruta de salida
+    $qrcode->render($_GET['url'], $path);
+    
+    // Redirige a la página que muestra el código QR generado
     header("Location: ../generated-qr.php?file=$name");
+    exit;
 } else {
+    // Redirige de vuelta a la página anterior si no se proporcionó una URL
     header("Location: ../qr-code.php");
+    exit;
 }
