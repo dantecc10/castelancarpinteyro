e7fc72bafcdf1b34b867b26415d059384a1ffe89
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-27 23:41:14 -0600

    ..

--- php scripts/create-qr.php
+++ php scripts/create-qr.php
@@ -8,8 +8,8 @@ if (isset($_GET['url'])) {
     $qrcode = new QRCode;
     $name = ("qr-" . time() . ".png");
     $path = ($name);
-    $qr_img = $qrcode->render($_GET['url'], (__DIR__ . "generated-qrs/" . $path));
-    //file_put_contents(("generated-qrs/" . $path), $qr_img);
+    $qr_img = $qrcode->render($_GET['url'], (__DIR__ . "/generated-qrs/" . $path));
+    //file_put_contents((__DIR__."generated-qrs/" . $path), $qr_img);
     header("Location: ../generated-qr.php?file=$name");
 } else {
     header("Location: ../qr-code.php");
