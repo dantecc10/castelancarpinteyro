9859272ba7a423e34af68230c727ad1d8b76904d
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-28 00:24:27 -0600

    ..

--- php scripts/qr-img.php
+++ php scripts/qr-img.php
@@ -1,25 +1,12 @@
 <?php
 
-if(isset($_GET['url'])){
-    $url = $_GET['url'];
-}else{
-    $url = "https://castelancarpinteyro.com";
-}
-/**
- * GdImage output example
- *
- * @created      24.12.2017
- * @author       Smiley <smiley@chillerlan.net>
- * @copyright    2017 Smiley
- * @license      MIT
- */
+require_once "../vendor/autoload.php";
 
 use chillerlan\QRCode\{QRCode, QROptions};
 use chillerlan\QRCode\Data\QRMatrix;
 use chillerlan\QRCode\Output\QRGdImagePNG;
 
-require_once "../vendor/autoload.php";
-
+// Inicializar las opciones del código QR
 $options = new QROptions;
 
 $options->version             = 7;
@@ -33,51 +20,50 @@ $options->drawCircularModules = true;
 $options->drawLightModules    = true;
 $options->circleRadius        = 0.4;
 $options->keepAsSquare        = [
-	QRMatrix::M_FINDER_DARK,
-	QRMatrix::M_FINDER_DOT,
-	QRMatrix::M_ALIGNMENT_DARK,
+    QRMatrix::M_FINDER_DARK,
+    QRMatrix::M_FINDER_DOT,
+    QRMatrix::M_ALIGNMENT_DARK,
 ];
 $options->moduleValues        = [
-	// finder
-	QRMatrix::M_FINDER_DARK    => [0, 63, 255], // dark (true)
-	QRMatrix::M_FINDER_DOT     => [0, 63, 255], // finder dot, dark (true)
-	QRMatrix::M_FINDER         => [233, 233, 233], // light (false), white is the transparency color and is enabled by default
-	// alignment
-	QRMatrix::M_ALIGNMENT_DARK => [255, 0, 255],
-	QRMatrix::M_ALIGNMENT      => [233, 233, 233],
-	// timing
-	QRMatrix::M_TIMING_DARK    => [255, 0, 0],
-	QRMatrix::M_TIMING         => [233, 233, 233],
-	// format
-	QRMatrix::M_FORMAT_DARK    => [67, 159, 84],
-	QRMatrix::M_FORMAT         => [233, 233, 233],
-	// version
-	QRMatrix::M_VERSION_DARK   => [62, 174, 190],
-	QRMatrix::M_VERSION        => [233, 233, 233],
-	// data
-	QRMatrix::M_DATA_DARK      => [0, 0, 0],
-	QRMatrix::M_DATA           => [233, 233, 233],
-	// darkmodule
-	QRMatrix::M_DARKMODULE     => [0, 0, 0],
-	// separator
-	QRMatrix::M_SEPARATOR      => [233, 233, 233],
-	// quietzone
-	QRMatrix::M_QUIETZONE      => [233, 233, 233],
-	// logo (requires a call to QRMatrix::setLogoSpace()), see QRImageWithLogo
-	QRMatrix::M_LOGO           => [233, 233, 233],
+    // finder
+    QRMatrix::M_FINDER_DARK    => [0, 63, 255], // dark (true)
+    QRMatrix::M_FINDER_DOT     => [0, 63, 255], // finder dot, dark (true)
+    QRMatrix::M_FINDER         => [233, 233, 233], // light (false), white is the transparency color and is enabled by default
+    // alignment
+    QRMatrix::M_ALIGNMENT_DARK => [255, 0, 255],
+    QRMatrix::M_ALIGNMENT      => [233, 233, 233],
+    // timing
+    QRMatrix::M_TIMING_DARK    => [255, 0, 0],
+    QRMatrix::M_TIMING         => [233, 233, 233],
+    // format
+    QRMatrix::M_FORMAT_DARK    => [67, 159, 84],
+    QRMatrix::M_FORMAT         => [233, 233, 233],
+    // version
+    QRMatrix::M_VERSION_DARK   => [62, 174, 190],
+    QRMatrix::M_VERSION        => [233, 233, 233],
+    // data
+    QRMatrix::M_DATA_DARK      => [0, 0, 0],
+    QRMatrix::M_DATA           => [233, 233, 233],
+    // darkmodule
+    QRMatrix::M_DARKMODULE     => [0, 0, 0],
+    // separator
+    QRMatrix::M_SEPARATOR      => [233, 233, 233],
+    // quietzone
+    QRMatrix::M_QUIETZONE      => [233, 233, 233],
+    // logo (requires a call to QRMatrix::setLogoSpace()), see QRImageWithLogo
+    QRMatrix::M_LOGO           => [233, 233, 233],
 ];
 
-
-$out = (new QRCode($options))->render($url);
-
-//header('Content-type: image/png');
-
-//echo $out;
+// Generar el código QR
+$out = (new QRCode($options))->render('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
 // Convertir imagen SVG a PNG
 $image = new Imagick();
 $image->readImageBlob($out);
 $image->setImageFormat("png24");
+
+// Redimensionar la imagen (por ejemplo, a 400x400 píxeles)
+$image->resizeImage(400, 400, Imagick::FILTER_LANCZOS, 1);
 
 // Guardar la imagen como archivo PNG
 $image->writeImage("generated-qrs/sample.png");
