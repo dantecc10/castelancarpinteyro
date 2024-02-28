a5e2e5731029b1d88d63e41a0f256b98e2e147b5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-02-27 23:29:40 -0600

    a ver

--- php scripts/create-qr.php
+++ php scripts/create-qr.php
@@ -1,15 +1,14 @@
 <?php
 
 use chillerlan\QRCode\QRCode;
-echo(__DIR__);
+
 if (isset($_GET['url'])) {
-    include "functions.php";
     include_once "../vendor/autoload.php";
-    $qrcode = new QRcode;
+    $qrcode = new QRCode;
     $qr_img = $qrcode->render($_GET['url']);
     $name = ("qr-" . time() . ".png");
     $path = ($name);
-    file_put_contents(("generated-qrs/".$path), $qr_img);
+    file_put_contents(("generated-qrs/" . $path), $qr_img);
     header("Location: ../generated-qr.php?file=$name");
 } else {
     header("Location: ../qr-code.php");
