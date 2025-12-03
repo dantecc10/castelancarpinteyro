12c9a2976705a7ac78bef55f0e0879d8a4af79ba
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-12-03 12:52:13 -0600

    algo

--- php-scripts/testESP32.php
+++ php-scripts/testESP32.php
@@ -1,6 +1,28 @@
 <?php
 include ('dynamicMailSettings.php');   // Importa tu archivo con setMailParameters()
 
+function setMailParameters($turing)
+{
+    include "dynamicSecrets.php";
+
+    $data = generatePasskey($turing);
+
+    //$mail->SMTPDebug    = 3;
+    $mail = new PHPMailer;
+    $mail->IsSMTP();
+    $mail->Host = "castelancarpinteyro.com"; #'classicandsacrum.com';   /*Servidor SMTP no pongas la ip, pon el nombre de la dns inversa*/																		
+    $mail->SMTPSecure = 'TLS';   /*Protocolo SSL o TLS*/
+    $mail->Port = 587;   /*Puerto de conexión al servidor SMTP*/
+    $mail->SMTPAuth = true;   /*Para habilitar o deshabilitar la autenticación*/
+    $mail->Username = $data[2]; #'academia@classicandsacrum.com';   /*Usuario, normalmente el correo electrónico*/
+    $mail->Password = $data[0];   /*Tu contraseña*/
+    $mail->From = $data[2]; #'academia@classicandsacrum.com';   /*Correo electrónico que estamos autenticando*/
+    $mail->FromName = $data[1];   /*Puedes poner tu nombre, el de tu empresa, nombre de tu web, etc.*/
+    $mail->CharSet = 'UTF-8';   /*Codificación del mensaje*/
+    //echo ($data[0] . " " . $data[1] . " " . $data[2]); // Debug command line
+    return $mail;
+}
+
 try {
     // Obtiene un PHPMailer ya configurado según tu función
     $mail = setMailParameters('tester');
